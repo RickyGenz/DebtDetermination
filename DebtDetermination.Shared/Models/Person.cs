0121af30e7cd14043065b7b583dec3081951653f
Ricky Genz <rgenz1@gmail.com>
2018-01-20 09:56:19 -0800

    Add Person.

---
+++ DebtDetermination.Shared/Models/Person.cs
@@ -0,0 +1,12 @@
+﻿namespace DebtDetermination.Shared.Models
+{
+    public class Person
+    {
+        public string Name { get; }
+
+        public Person(string name)
+        {
+            this.Name = name;
+        }
+    }
+}
---
+++ DebtDetermination.Tests/Models/PersonTest.cs
@@ -0,0 +1,25 @@
+﻿using DebtDetermination.Shared.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DebtDetermination.Tests.Models
+{
+    [TestClass]
+    public class PersonTest
+    {
+        string Name;
+        Person Person;
+
+        [TestInitialize]
+        public void InstantiatePerson()
+        {
+            Name = "Ricky";
+            Person = new Person(Name);
+        }
+
+        [TestMethod]
+        public void PersonHasName()
+        {
+            Assert.AreEqual(Name, Person.Name);
+        }
+    }
+}
