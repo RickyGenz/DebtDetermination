9d6e25e0064d374ce1433128d7eb6b96bbd6d897
Ricky Genz <rgenz1@gmail.com>
2018-01-25 19:09:35 -0800

    Add Transaction.

---
+++ DebtDetermination.Shared/Models/Transaction.cs
@@ -0,0 +1,14 @@
+﻿namespace DebtDetermination.Shared.Models
+{
+    public class Transaction
+    {
+        public string Name { get; }
+        public decimal Amount { get; }
+
+        public Transaction(string name, decimal amount)
+        {
+            this.Name = name;
+            this.Amount = amount;
+        }
+    }
+}
---
+++ DebtDetermination.Tests/Models/TransactionTest.cs
@@ -0,0 +1,33 @@
+﻿using DebtDetermination.Shared.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DebtDetermination.Tests.Models
+{
+    [TestClass]
+    public class TransactionTest
+    {
+        string Name;
+        decimal Amount;
+        Transaction Transaction;
+
+        [TestInitialize]
+        public void InstantiatePerson()
+        {
+            Name = "Pi";
+            Amount = 3.14m;
+            Transaction = new Transaction(Name, Amount);
+        }
+
+        [TestMethod]
+        public void TransactionHasName()
+        {
+            Assert.AreEqual(Name, Transaction.Name);
+        }
+
+        [TestMethod]
+        public void TransactionHasAmount()
+        {
+            Assert.AreEqual(Amount, Transaction.Amount);
+        }
+    }
+}
